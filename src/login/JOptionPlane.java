31506e3f140fd674a6e3ff91f90d9b8af7a620b6
AltroN-200 <akashmadavi2001@gmail.com>
2025-07-05 10:09:00 +0530

    first commit

---
+++ src/login/JOptionPlane.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+/**
+ *
+ * @author AKASH
+ */
+class JOptionPlane {
+
+    static void showMessageDailog(Object object, String please_enter_the_name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessageDialog(Object object, String please_enter_the_name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
